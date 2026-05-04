b3d8375b11c62d79588da58260635359f0b4ea9f
hoang160102 <hoang160102@gmail.com>
2026-05-04 21:52:42 +0700

    feat: logout

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -6,5 +6,6 @@ const router = Router();
 
 router.post('/login', AuthController.login);
 router.get('/me', protect, AuthController.getMe);
+router.get('/logout', AuthController.logout);
 
 export default router;
